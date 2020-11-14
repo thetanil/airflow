apache/spark

Optimizes CI builds heavily with selective checks (#11656)
* Images are not built if the change is not touching code or docs.
* In case we have no need for CI images we run stripped-down
  pre-commit checks which skip the long checks and only run for
  changed files
* If none of the CLI/Providers/Kubernetes/WWW files changed
  the relevant tests are skipped, unless some of the core files
  changed as well.
* The selective checks logic is explained and documented.

This is the second attempt at the problem with better
strategy to get the list of files from the incoming PR.

The strategy works now better in a number of cases:
* when PR comes from the same repo
* when PR comes from the pull_repo
* when PR contains more than one commit
* when PR is based on older master and GitHub creates
  merge commit

scripts/ci/selective_ci_checks.sh
@@ -18,12 +18,24 @@
 # shellcheck source=scripts/ci/libraries/_script_init.sh
 . ./scripts/ci/libraries/_script_init.sh
 
+# Parameter:
+#
+# $1 - COMMIT SHA of the incoming commit. If this parameter is missing, this script does not check anything,
+#      it simply sets all the version outputs that determine that all tests should be run.
+#      This happens in case the even triggering the workflow is 'schedule' or 'push'.
+#
+# The logic of retrieving changes works by comparing the incoming commit with the target branch
+# The commit addresses.
+#
+#
 declare -a pattern_array
 
 function output_all_basic_variables() {
     initialization::ga_output python-versions \
         "$(initialization::parameters_to_json "${CURRENT_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
     initialization::ga_output default-python-version "${DEFAULT_PYTHON_MAJOR_MINOR_VERSION}"
+    initialization::ga_output all-python-versions \
+        "$(initialization::parameters_to_json "${ALL_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
 
     initialization::ga_output kubernetes-versions \
         "$(initialization::parameters_to_json "${CURRENT_KUBERNETES_VERSIONS[@]}")"
@@ -58,34 +70,83 @@ function output_all_basic_variables() {
     initialization::ga_output kubernetes-exclude '[]'
 }
 
-function set_outputs_run_all_tests() {
-    initialization::ga_output run-tests "true"
-    initialization::ga_output run-kubernetes-tests "true"
-    initialization::ga_output test-types "Core Other API CLI Providers WWW Integration Heisentests"
-}
+function get_changed_files() {
+    INCOMING_COMMIT_SHA="${1}"
+    readonly INCOMING_COMMIT_SHA
 
-function set_output_skip_all_tests() {
-    initialization::ga_output run-tests "false"
-    initialization::ga_output run-kubernetes-tests "false"
-    initialization::ga_output test-types ""
-}
-
-function initialize_git_repo() {
-    git remote add target "https://github.com/${CI_TARGET_REPO}"
-    git fetch target "${CI_TARGET_BRANCH}:${CI_TARGET_BRANCH}" --depth=1
     echo
-    echo "My commit SHA: ${COMMIT_SHA}"
+    echo "Incoming commit SHA: ${INCOMING_COMMIT_SHA}"
     echo
+    echo "Changed files from ${INCOMING_COMMIT_SHA} vs it's first parent"
     echo
-    echo "Retrieved changed files from ${COMMIT_SHA} comparing to ${CI_TARGET_BRANCH} in ${CI_TARGET_REPO}"
-    echo
-    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r "${COMMIT_SHA}" "${CI_TARGET_BRANCH}" || true)
+    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only \
+        -r "${INCOMING_COMMIT_SHA}^" "${INCOMING_COMMIT_SHA}" || true)
+    if [[ ${CHANGED_FILES} == "" ]]; then
+        >&2 echo
+        >&2 echo Warning! Could not find any changed files
+        >&2 echo Assuming that we should run all tests in this case
+        >&2 echo
+        set_outputs_run_everything_and_exit
+    fi
     echo
     echo "Changed files:"
     echo
     echo "${CHANGED_FILES}"
     echo
     readonly CHANGED_FILES
+}
+
+
+function run_tests() {
+    initialization::ga_output run-tests "${@}"
+}
+
+function run_kubernetes_tests() {
+    initialization::ga_output run-kubernetes-tests "${@}"
+}
+
+function needs_helm_tests() {
+    initialization::ga_output needs-helm-tests "${@}"
+}
+
+function needs_api_tests() {
+    initialization::ga_output needs-api-tests "${@}"
+}
+
+function set_test_types() {
+    initialization::ga_output test-types "${@}"
+}
+
+function set_basic_checks_only() {
+    initialization::ga_output basic-checks-only "${@}"
+}
+
+ALL_TESTS="Core Other API CLI Providers WWW Integration Heisentests"
+readonly ALL_TESTS
+
+function set_outputs_run_everything_and_exit() {
+    needs_api_tests "true"
+    needs_helm_tests "true"
+    run_tests "true"
+    run_kubernetes_tests "true"
+    set_test_types "${ALL_TESTS}"
+    set_basic_checks_only "false"
+    exit
+}
+
+function set_outputs_run_all_tests() {
+    run_tests "true"
+    run_kubernetes_tests "true"
+    set_test_types "${ALL_TESTS}"
+    set_basic_checks_only "false"
+}
+
+function set_output_skip_all_tests_and_exit() {
+    run_tests "false"
+    run_kubernetes_tests "false"
+    set_test_types ""
+    set_basic_checks_only "true"
+    exit
 }
 
 # Converts array of patterns into single | pattern string
@@ -121,76 +182,91 @@ function show_changed_files() {
 # Output:
 #    Count of changed files matching the patterns
 function count_changed_files() {
-    count_changed_files=$(echo "${CHANGED_FILES}" | grep -c -E "$(get_regexp_from_patterns)" || true)
-    echo "${count_changed_files}"
+    echo "${CHANGED_FILES}" | grep -c -E "$(get_regexp_from_patterns)" || true
 }
 
-function run_all_tests_when_push_or_schedule() {
-    if [[ ${GITHUB_EVENT_NAME} == "push" || ${GITHUB_EVENT_NAME} == "schedule" ]]; then
-        echo
-        echo "Always run all tests in case of push/schedule events"
-        echo
-        set_outputs_run_all_tests
-        exit
+function check_if_api_tests_should_be_run() {
+    local pattern_array=(
+        "^airflow/api"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        needs_api_tests "false"
+    else
+        needs_api_tests "true"
     fi
 }
 
-function check_if_tests_should_be_run_at_all() {
-    TEST_TRIGGERING_PATTERNS=(
-        "^airflow"
-        "^.github/workflows/"
-        "^Dockerfile"
-        "^scripts"
+function check_if_helm_tests_should_be_run() {
+    local pattern_array=(
         "^chart"
-        "^setup.py"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        needs_helm_tests "false"
+    else
+        needs_helm_tests "true"
+    fi
+}
+
+function check_if_docs_should_be_generated() {
+    local pattern_array=(
+        "^docs$"
+        "^airflow/.*\.py$"
+        "^CHANGELOG\.txt"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        echo "None of the docs changed"
+    else
+        image_build_needed="true"
+    fi
+}
+
+AIRFLOW_SOURCES_TRIGGERING_TESTS=(
+        "^airflow"
+        "^chart"
         "^tests"
         "^kubernetes_tests"
-    )
-    readonly TEST_TRIGGERING_PATTERNS
+)
+readonly AIRFLOW_SOURCES_TRIGGERING_TESTS
 
-    pattern_array=("${TEST_TRIGGERING_PATTERNS[@]}")
-    show_changed_files "$(get_regexp_from_patterns)"
+function check_if_tests_are_needed_at_all() {
+    local pattern_array=("${AIRFLOW_SOURCES_TRIGGERING_TESTS[@]}")
+    show_changed_files
 
     if [[ $(count_changed_files) == "0" ]]; then
         echo "None of the important files changed, Skipping tests"
-        set_output_skip_all_tests
-        exit
+        set_output_skip_all_tests_and_exit
     else
-        initialization::ga_output run-tests "true"
+        image_build_needed="true"
+        tests_needed="true"
     fi
 }
 
-function check_if_environment_files_changed() {
-    ENVIRONMENT_TRIGGERING_PATTERNS=(
+function run_all_tests_if_environment_files_changed() {
+    local pattern_array=(
         "^.github/workflows/"
         "^Dockerfile"
         "^scripts"
         "^setup.py"
     )
-    readonly ENVIRONMENT_TRIGGERING_PATTERNS
-
-    pattern_array=("${ENVIRONMENT_TRIGGERING_PATTERNS[@]}")
-    show_changed_files "$(get_regexp_from_patterns)"
+    show_changed_files
 
     if [[ $(count_changed_files) != "0" ]]; then
-        echo "Important environment files changed. Running all tests"
-        set_outputs_run_all_tests
-        exit
+        echo "Important environment files changed. Running everything"
+        set_outputs_run_everything_and_exit
     fi
 }
 
 function get_count_all_files() {
     echo
-    echo "Count All files"
+    echo "Count All airflow source files"
     echo
-    ALL_TRIGGERING_PATTERNS=(
-        "^airflow"
-        "^chart"
-        "^tests"
-        "^kubernetes_tests"
-    )
-    readonly ALL_TRIGGERING_PATTERNS
-    pattern_array=("${ALL_TRIGGERING_PATTERNS[@]}")
+    local pattern_array=("${AIRFLOW_SOURCES_TRIGGERING_TESTS[@]}")
     show_changed_files
     COUNT_ALL_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_ALL_CHANGED_FILES}"
@@ -201,14 +277,12 @@ function get_count_api_files() {
     echo
     echo "Count API files"
     echo
-    API_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/api"
         "^airflow/api_connexion"
         "^tests/api"
         "^tests/api_connexion"
     )
-    readonly API_TRIGGERING_PATTERNS
-    pattern_array=("${API_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_API_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_API_CHANGED_FILES}"
@@ -219,12 +293,10 @@ function get_count_cli_files() {
     echo
     echo "Count CLI files"
     echo
-    CLI_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/cli"
         "^tests/cli"
     )
-    readonly CLI_TRIGGERING_PATTERNS
-    pattern_array=("${CLI_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_CLI_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_CLI_CHANGED_FILES}"
@@ -235,12 +307,10 @@ function get_count_providers_files() {
     echo
     echo "Count Providers files"
     echo
-    PROVIDERS_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/providers"
         "^tests/providers"
     )
-    readonly PROVIDERS_TRIGGERING_PATTERNS
-    pattern_array=("${PROVIDERS_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_PROVIDERS_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_PROVIDERS_CHANGED_FILES}"
@@ -251,12 +321,10 @@ function get_count_www_files() {
     echo
     echo "Count WWW files"
     echo
-    WWW_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/www"
         "^tests/www"
     )
-    readonly WWW_TRIGGERING_PATTERNS
-    pattern_array=("${WWW_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_WWW_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_WWW_CHANGED_FILES}"
@@ -267,13 +335,11 @@ function get_count_kubernetes_files() {
     echo
     echo "Count Kubernetes files"
     echo
-    KUBERNETES_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/kubernetes"
         "^chart"
         "^tests/kubernetes_tests"
     )
-    readonly KUBERNETES_TRIGGERING_PATTERNS
-    pattern_array=("${KUBERNETES_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_KUBERNETES_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_KUBERNETES_CHANGED_FILES}"
@@ -284,9 +350,7 @@ function calculate_test_types_to_run() {
     echo
     echo "Count Core/Other files"
     echo
-    COUNT_CORE_OTHER_CHANGED_FILES=$((COUNT_ALL_CHANGED_FILES - COUNT_WWW_CHANGED_FILES - \
-        COUNT_PROVIDERS_CHANGED_FILES - COUNT_CLI_CHANGED_FILES - COUNT_API_CHANGED_FILES - \
-        COUNT_KUBERNETES_CHANGED_FILES))
+    COUNT_CORE_OTHER_CHANGED_FILES=$((COUNT_ALL_CHANGED_FILES - COUNT_WWW_CHANGED_FILES - COUNT_PROVIDERS_CHANGED_FILES - COUNT_CLI_CHANGED_FILES - COUNT_API_CHANGED_FILES - COUNT_KUBERNETES_CHANGED_FILES))
 
     readonly COUNT_CORE_OTHER_CHANGED_FILES
     echo
@@ -301,11 +365,9 @@ function calculate_test_types_to_run() {
         set_outputs_run_all_tests
     else
         if [[ ${COUNT_KUBERNETES_CHANGED_FILES} != "0" ]]; then
-            initialization::ga_output run-kubernetes-tests "true"
-        else
-            initialization::ga_output run-kubernetes-tests "false"
+            kubernetes_tests_needed="true"
         fi
-        initialization::ga_output run-tests "true"
+        tests_needed="true"
         SELECTED_TESTS=""
         if [[ ${COUNT_API_CHANGED_FILES} != "0" ]]; then
             echo
@@ -336,10 +398,24 @@ function calculate_test_types_to_run() {
 }
 
 output_all_basic_variables
-run_all_tests_when_push_or_schedule
-initialize_git_repo
-check_if_tests_should_be_run_at_all
-check_if_environment_files_changed
+
+if (($# < 1)); then
+    echo
+    echo "No Commit SHA - running all tests!"
+    echo
+    set_outputs_run_everything_and_exit
+fi
+
+image_build_needed="false"
+tests_needed="false"
+kubernetes_tests_needed="false"
+
+get_changed_files "${1}"
+run_all_tests_if_environment_files_changed
+check_if_docs_should_be_generated
+check_if_helm_tests_should_be_run
+check_if_api_tests_should_be_run
+check_if_tests_are_needed_at_all
 get_count_all_files
 get_count_api_files
 get_count_cli_files
@@ -347,3 +423,21 @@ get_count_providers_files
 get_count_www_files
 get_count_kubernetes_files
 calculate_test_types_to_run
+
+if [[ ${image_build_needed} == "true" ]]; then
+    set_basic_checks_only "false"
+else
+    set_basic_checks_only "true"
+fi
+
+if [[ ${tests_needed} == "true" ]]; then
+    run_tests
+else
+    skip_running_tests
+fi
+
+if [[ ${kubernetes_tests_needed} == "true" ]]; then
+    run_kubernetes_tests
+else
+    skip_running_kubernetes_tests
+fi

thetanil/airflow

(cherry picked from commit dd1c07b20d9037527b2979b72013ecd6b0caf1cd)

scripts/ci/selective_ci_checks.sh
@@ -20,10 +20,13 @@
 
 # Parameter:
 #
-# $1 - Merge commit SHA. If this parameter is missing, this script does not check anything, it simply
-#      sets all the version outputs that determine that all tests should be run. This happens in case
-#      the even triggering the workflow is 'schedule' or 'push'. Merge commit is only
-#      available in case of 'pull_request' triggered runs.
+# $1 - COMMIT SHA of the incoming commit. If this parameter is missing, this script does not check anything,
+#      it simply sets all the version outputs that determine that all tests should be run.
+#      This happens in case the even triggering the workflow is 'schedule' or 'push'.
+#
+# The logic of retrieving changes works by comparing the incoming commit with the target branch
+# The commit addresses.
+#
 #
 declare -a pattern_array
 
@@ -67,6 +70,66 @@ function output_all_basic_variables() {
     initialization::ga_output kubernetes-exclude '[]'
 }
 
+function get_changed_files() {
+    INCOMING_COMMIT_SHA="${1}"
+    readonly INCOMING_COMMIT_SHA
+
+    echo
+    echo "Incoming commit SHA: ${INCOMING_COMMIT_SHA}"
+    echo
+    echo "Changed files from ${INCOMING_COMMIT_SHA} vs it's first parent"
+    echo
+    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only \
+        -r "${INCOMING_COMMIT_SHA}^" "${INCOMING_COMMIT_SHA}" || true)
+    if [[ ${CHANGED_FILES} == "" ]]; then
+        >&2 echo
+        >&2 echo Warning! Could not find any changed files
+        >&2 echo Assuming that we should run all tests in this case
+        >&2 echo
+        set_outputs_run_everything_and_exit
+    fi
+    echo
+    echo "Changed files:"
+    echo
+    echo "${CHANGED_FILES}"
+    echo
+    readonly CHANGED_FILES
+}
+
+
+function run_tests() {
+    initialization::ga_output run-tests "${@}"
+}
+
+function run_kubernetes_tests() {
+    initialization::ga_output run-kubernetes-tests "${@}"
+}
+
+function needs_helm_tests() {
+    initialization::ga_output needs-helm-tests "${@}"
+}
+
+function set_test_types() {
+    initialization::ga_output test-types "${@}"
+}
+
+function set_basic_checks_only() {
+    initialization::ga_output basic-checks-only "${@}"
+}
+
+ALL_TESTS="Core Integration Heisentests"
+readonly ALL_TESTS
+
+function set_outputs_run_everything_and_exit() {
+    needs_api_tests "true"
+    needs_helm_tests "true"
+    run_tests "true"
+    run_kubernetes_tests "true"
+    set_test_types "${ALL_TESTS}"
+    set_basic_checks_only "false"
+    exit
+}
+
 function set_outputs_run_all_tests() {
     run_tests "true"
     run_kubernetes_tests "true"
@@ -80,20 +143,6 @@ function set_output_skip_all_tests_and_exit() {
     set_test_types ""
     set_basic_checks_only "true"
     exit
-}
-
-function get_changed_files() {
-    local commit_sha=${1}
-    echo
-    echo "Retrieved changed files from ${commit_sha}"
-    echo
-    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r "${commit_sha}" "${commit_sha}^" || true)
-    echo
-    echo "Changed files:"
-    echo
-    echo "${CHANGED_FILES}"
-    echo
-    readonly CHANGED_FILES
 }
 
 # Converts array of patterns into single | pattern string
@@ -129,9 +178,7 @@ function show_changed_files() {
 # Output:
 #    Count of changed files matching the patterns
 function count_changed_files() {
-    local count_changed_files
-    count_changed_files=$(echo "${CHANGED_FILES}" | grep -c -E "$(get_regexp_from_patterns)" || true)
-    echo "${count_changed_files}"
+    echo "${CHANGED_FILES}" | grep -c -E "$(get_regexp_from_patterns)" || true
 }
 
 function run_all_tests_when_push_or_schedule() {
@@ -160,7 +207,7 @@ function check_if_helm_tests_should_be_run() {
 function check_if_docs_should_be_generated() {
     local pattern_array=(
         "^docs$"
-        "\.py$"
+        "^airflow/.*\.py$"
         "^CHANGELOG\.txt"
     )
     show_changed_files
@@ -264,23 +311,16 @@ output_all_basic_variables
 
 if (($# < 1)); then
     echo
-    echo "No merge commit SHA - running all tests!"
+    echo "No Commit SHA - running all tests!"
     echo
     set_outputs_run_everything_and_exit
 fi
-
-MERGE_COMMIT_SHA="${1}"
-readonly MERGE_COMMIT_SHA
-
-echo
-echo "Merge commit SHA: ${MERGE_COMMIT_SHA}"
-echo
 
 image_build_needed="false"
 tests_needed="false"
 kubernetes_tests_needed="false"
 
-get_changed_files "${MERGE_COMMIT_SHA}"
+get_changed_files "${1}"
 run_all_tests_if_environment_files_changed
 check_if_docs_should_be_generated
 check_if_helm_tests_should_be_run